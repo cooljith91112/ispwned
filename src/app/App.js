e4cc7072b439e5dcbfbdff203683b1a1598eb123
Indrajith K L <mac91112@gmail.com>
2018-08-03 14:35:05 +0530

    * Konami code.

--- src/app/App.js
+++ src/app/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import SearchField from './SearchField';
 import vex from 'vex-js/dist/js/vex.combined';
 import SoundCloudAudio from 'soundcloud-audio';
-
+import './index.css';
 export default class App extends Component {
     
     constructor(props){
@@ -14,15 +14,19 @@ export default class App extends Component {
         }
 
         this.playMusic = this.playMusic.bind(this);
+        this.state = {
+            konamiKlass: ''
+        };
     }
 
     componentDidMount(){
         vex.dialog.alert('This website is for educational purpose only. Every data is fetched from haveibeenpwned.com');
+        this.konamiListener();
     }
     
     render(){
         return(
-            <div id="content">
+            <div id="content" className={`${this.state.konamiKlass}`}>
             <SearchField />
             </div>
         );
@@ -40,6 +44,24 @@ export default class App extends Component {
 
                 });
             });
+        }
+    }
+
+    konamiListener(){
+        if ( window.addEventListener ) {
+            var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+            window.addEventListener("keydown", (e)=>{
+                kkeys.push( e.keyCode );
+                // debugger;
+                if ( kkeys.toString().indexOf( konami ) >= 0 ){
+                    // console.log('Aila');
+                    this.setState({
+                        konamiKlass:'konami'
+                    });
+                    this.playMusic();
+                    kkeys = [];
+        }
+            }, true);
         }
     }
 }
